f01a5993178baa279562ea052ab550445ef057ea
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-17 16:15:43 +0600

    add inner html table data

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello index js');
+
+document.getElementById('first-card').addEventListener('click',function(){
+    const productName = document.getElementById('first-name').innerText;
+    const productPrice = document.getElementById('first-price').innerText;
+    const productQuantity = document.getElementById('first-quantity').innerText;
+    // console.log(productElement,productPrice,productQuantity)
+
+    const container = document.getElementById('table-container');
+
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${productName}</td>
+    <td>${productPrice}</td>
+    <td>${productQuantity}</td>
+    `;
+    container.appendChild(tr);
+})
