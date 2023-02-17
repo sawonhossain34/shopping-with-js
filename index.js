ae4ceec2e277bd1b0a818402a7c565e78e3222b5
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-17 18:00:56 +0600

    styem Node add 2nd

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 
 document.getElementById('first-card').addEventListener('click',function(){
+    // get the data
     const productName = document.getElementById('first-name').innerText;
     const productPrice = document.getElementById('first-price').innerText;
     const productQuantity = document.getElementById('first-quantity').innerText;
     // console.log(productElement,productPrice,productQuantity)
 
+    const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
+    // console.log(priceTotal);
+  
+    
+   // show the data
+   displayData(productName,productPrice,productQuantity,priceTotal);
+})
+
+document.getElementById('secound-card').addEventListener('click',function(e){
+    // console.log(e.target.parentNode.parentNode.children[0].innerText);
+    const pName = e.target.parentNode.parentNode.children[0].innerText;
+    const pPrice = e.target.parentNode.parentNode.children[2].children[0].innerText;
+    const pQuantity = e.target.parentNode.parentNode.children[3].children[0].innerText;
+    //  console.log(productName,productPrice,productQuantity);
+
+    const sumTotal = parseInt(pPrice) + parseInt(pQuantity);
+    
+    // console.log(sumTotal);
+    displayData(pName,pPrice,pQuantity,sumTotal);
+})
+
+// common function//
+function displayData(productName,productPrice,productQuantity,priceTotal){
     const container = document.getElementById('table-container');
 
     const tr = document.createElement("tr")
@@ -13,6 +37,7 @@ document.getElementById('first-card').addEventListener('click',function(){
     <td>${productName}</td>
     <td>${productPrice}</td>
     <td>${productQuantity}</td>
+    <td>${priceTotal}</td>
     `;
     container.appendChild(tr);
-})
+}
